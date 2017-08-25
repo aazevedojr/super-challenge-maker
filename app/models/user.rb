96a1c4ecb0528c59b4066a4df3166f2e70c929ab
Augusto Azevedo <augusto.azevedo.jr@gmail.com>
2017-08-25 15:18:52 -0700

    User model and errors partial

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   has_many :courses, foreign_key: :maker_id
   has_many :reviews, foreign_key: :reviewer_id
+  has_many :challenges, foreign_key: :proposer_id
 
   validates :email, :username, {uniqueness: true, presence: true}
 
